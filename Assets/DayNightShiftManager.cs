a673b5f3871ea78bff071b14633a56497cd1e928
vegiwoo <vegiwoo@protonmail.com>
2022-08-25 13:13:27 +0300

    Work commit

--- Assets/DayNightShiftManager.cs
+++ Assets/DayNightShiftManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -40,6 +41,31 @@ namespace C03_Adventure.Managers
             dayDuration = 30f;
 
             StartCoroutine(TimeOfDayCoroutine());
+        }
+
+        public Color myColor;
+        private void OnGUI()
+        {
+            myColor = RGBSlider(new Rect(10, 10, 200, 20), myColor);
+        }
+
+        private Color RGBSlider(Rect screenRect, Color rgb)
+        {
+            rgb.r = LabelSlider(screenRect, rgb.r, 1.0f, "Red");
+            screenRect.y += 20;
+            rgb.g = LabelSlider(screenRect, rgb.g, 1.0f, "Green");
+            screenRect.y += 20;
+            rgb.b = LabelSlider(screenRect, rgb.b, 1.0f, "Blue");
+            return rgb;
+        }
+        
+        private static float LabelSlider(Rect screenRect, float sliderValue, float sliderMaxValue, string labelText)
+        {
+            GUI.Label(screenRect, labelText);
+            screenRect.x += screenRect.width;
+         
+            sliderValue = GUI.HorizontalSlider(screenRect, sliderValue, 0.0f, sliderMaxValue);
+            return sliderValue;
         }
 
         private IEnumerator TimeOfDayCoroutine()
